ad0bfe5173c47cf4b9b7a7b58d76b9f4d81783d5
Andrii <amudrevskiy@gmail.com>
2016-11-08 22:46:05 +0200

    module 3

---
+++ src/module3/adsWebSite/Main.java
@@ -0,0 +1,13 @@
+package module3.adsWebSite;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //
+        //User user1 = new User("Jack", "Kop", );
+
+
+
+    }
+
+}
---
+++ src/module3/phones/IPhone7.java
@@ -0,0 +1,10 @@
+package module3.phones;
+
+public class IPhone7 extends IPhone {
+
+    public IPhone7(int cost, String model, String manufacturer) {
+        super(cost, model, manufacturer);
+    }
+
+
+}
